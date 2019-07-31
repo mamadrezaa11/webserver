7f6c1816c0e34573180b90d33efd943ab4b3bd68
mamadrezaa11 <50425906+mamadrezaa11@users.noreply.github.com>
2019-07-31 16:06:43 -0400

    Update purenodeserver.js

--- purenodeserver.js
+++ purenodeserver.js
@@ -1,10 +1,44 @@
 const http = require('http');
-
-function handler(req, res){
-    res.writehead(200, {'content-type':'text/plain'});
-    res.write('hello, I a, a webserver!');
-    res.end();
+const url = require('url');
+ 
+function handler(req, res) {
+    const parsedUrl = url.parse(req.url, true);
+ 
+    res.setHeader('x-server-date', new Date());
+ 
+    if(parsedUrl.pathname === '/') {
+        res.writeHead(200, {'Content-type':'text/plain'});
+        res.write('Hello, I am a webserver!');
+        return res.end();
+    } else if(parsedUrl.pathname === '/time') {
+        res.writeHead(200, {'Content-type':'text/plain'});
+        res.write(new Date().toString());
+        return res.end();
+    } else if(parsedUrl.pathname === '/hello') {
+        const name = parsedUrl.query.name;
+        if(!name) {
+            res.writeHead(400, {'Content-type':'text/plain'});
+            return res.end();  
+        }
+        res.writeHead(200, {'Content-type':'text/plain'});
+        res.write(`Hello ${name}`);
+        return res.end();
+    } else if(parsedUrl.pathname.startsWith('/user/')) {
+        const regex = new RegExp('\/user\/(.+)');
+        const matches = regex.exec(parsedUrl.pathname);
+        if(!matches || !matches[1]) {
+            res.writeHead(400, {'Content-type':'text/plain'});
+            return res.end();  
+        }
+        res.writeHead(200, {'Content-type':'text/plain'});
+        res.write(`Userpofile of ${matches[1]}`);
+        return res.end();
+    } else {
+        res.writeHead(404, {'Content-type':'text/plain'});
+        return res.end();
+    }
 }
-const server = http.createServer{handler};
-
-server.Listen(3000);
+ 
+const server = http.createServer(handler);
+ 
+server.listen(3000);
